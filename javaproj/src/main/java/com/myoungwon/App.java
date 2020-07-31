99ce3e1848ea16e6b7c58cc5905fc8f0d067f572
purelove2u <59471889+purelove2u@users.noreply.github.com>
2020-07-31 17:09:17 +0900

    12강까지 완강

--- javaproj/src/main/java/com/myoungwon/App.java
+++ javaproj/src/main/java/com/myoungwon/App.java
@@ -1,5 +1,7 @@
 package com.myoungwon;
 
+import com.myoungwon.examlib.entity.Exam;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Maven!" );
+        System.out.println( "Hello Eclipse Maven!" );
+        Exam exam = new Exam(10, 20, 30);
+        System.out.println(exam.total());
     }
+    
 }
